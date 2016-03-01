3b0ee78cf4b4f3802fd4e7e913cc0a23db27c4b8
Thomas Ruoff <ThomasRuoff@gmail.com>
2016-03-01 23:37:55 +0100

    ignore missing config file, but throw other errors

--- config.js
+++ config.js
@@ -11,6 +11,9 @@ let config = Object.assign({}, defaults);
 try {
     config = Object.assign(config, require(configPath));
 } catch(err) {
+    if (err.code !== 'MODULE_NOT_FOUND') {
+        throw err;
+    }
     // No config found in "${configPath}". Using defaults...
 }
 
